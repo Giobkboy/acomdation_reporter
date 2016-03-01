d37c1c75d3e6a0c8c1281d7371628fa980655ed3
afseAdmin <afseAdmin@AFSE-Mac-527.local>
2016-03-01 13:52:25 -0500

    2nd push

---
+++ Disablity.php
@@ -0,0 +1,20 @@
+<?php
+	
+
+	class Disablity{
+
+		private $disablity_name;
+		private $id;
+		private $discription; 
+
+		private $modifcations = array();
+
+		function __construct(){
+			//loads from db
+
+		}
+
+
+	}
+
+?>
